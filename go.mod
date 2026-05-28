54dd039626d741fc5e426fe1bfad1794c99cb505
xfac11 <xfac115@gmail.com>
2026-05-28 19:46:49 +0200

    sql queries and schemas are defined in the sql folder and then the GO code for it is generated by sqlc using sqlc.yaml file and the queries folder inside sql folder. goose is used to make up and down migrations. Login and register are new commands

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/xfac11/gator
 
 go 1.26.2
+
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lib/pq v1.12.3 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/google/uuid"
+	_ "github.com/lib/pq"
 	"github.com/xfac11/gator/internal/config"
+	"github.com/xfac11/gator/internal/database"
 )
 
 type state struct {
+	db            *database.Queries
 	configHandler *config.Config
 }
 
@@ -16,15 +23,61 @@ type command struct {
 	arguments []string
 }
 
+type commands struct {
+	plan map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	cmdFunc, ok := c.plan[cmd.name]
+	if !ok {
+		return fmt.Errorf("No command registered with that name")
+	}
+	return cmdFunc(s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.plan[name] = f
+}
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.arguments) == 0 {
-		return fmt.Errorf("Argument slice empty. Excepts one argument")
+		return fmt.Errorf("error: Excepts one username as argument")
 	}
-	err := s.configHandler.SetUser(cmd.arguments[0])
+	_, err := s.db.GetUser(context.Background(), cmd.arguments[0])
+	if err != nil {
+		return fmt.Errorf("No user is registered with that name")
+	}
+	err = s.configHandler.SetUser(cmd.arguments[0])
 	if err != nil {
 		return err
 	}
 	fmt.Println("User has been set to:", cmd.arguments[0])
+	return nil
+}
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return fmt.Errorf("error: Excepts one name as argument")
+	}
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.arguments[0],
+	}
+	user, err := s.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return fmt.Errorf("User with that name already exists, Error: %w", err)
+	}
+	err = s.configHandler.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("Could not set config, error: %w", err)
+	}
+
+	fmt.Println("User was created with name", user.Name)
+	fmt.Println("id:", user.ID)
+	fmt.Println("created_at:", user.CreatedAt)
+	fmt.Println("updated_at:", user.UpdatedAt)
+	fmt.Println("name:", user.Name)
+
 	return nil
 }
 func main() {
@@ -33,19 +86,39 @@ func main() {
 		fmt.Println("Could not read config. Error: %w", err)
 		os.Exit(1)
 	}
-	name := "filip"
-	err = cfg.SetUser(name)
+	db, err := sql.Open("postgres", cfg.DatabaseUrl)
 	if err != nil {
-		fmt.Printf("Could not set user in config to %s. Error: /n", name)
 		fmt.Println(err)
 		os.Exit(1)
 	}
+	dbQueries := database.New(db)
 
-	cfg, err = config.Read()
-	if err != nil {
-		fmt.Println("Could not read config. Error: %w", err)
+	newState := state{
+		configHandler: &cfg,
+		db:            dbQueries,
+	}
+
+	commands := commands{
+		plan: map[string]func(*state, command) error{},
+	}
+
+	commands.register("login", handlerLogin)
+	commands.register("register", handlerRegister)
+	if len(os.Args) < 2 {
+		fmt.Println("No command given")
 		os.Exit(1)
 	}
 
-	fmt.Println("Database url:", cfg.DatabaseUrl, "Username:", cfg.CurrentUserName)
+	name := os.Args[1]
+	arguments := os.Args[2:]
+
+	cmd := command{
+		name:      name,
+		arguments: arguments,
+	}
+	err = commands.run(&newState, cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
